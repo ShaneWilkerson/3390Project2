647291baa88d851f6b937412c6a57c2a38cae459
FantasticPilot <dantecarrizales2@gmail.com>
2025-10-16 20:22:40 -0700

    Added dynamic text that changes depending on logged in status + Added buttons with unique IDs for each gym in the database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,53 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import colors from "../../constants/colors";
+import { ProfileContext } from "../context/ProfileContext";
 
 // for database
-
+import { loadGyms } from "../../database";
+type Gym = {
+  id: number;
+  name: string;
+  address: string;
+};
 
 export default function HomeScreen() {
+
+  const { profile } = useContext(ProfileContext);
+  const [gyms, setGyms] = useState<Gym[]>([]);
+  const username = profile?.username || "Guest";
+  const message = profile? "Search and review gyms near you." : "Please login to post reviews for gyms near you.";
+
+  useEffect(() => {
+    const fetchGyms = async () => {
+      const gymsFromDB = await loadGyms();
+      setGyms(gymsFromDB);
+    };
+    fetchGyms();
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}> Swolemates</Text>
       </View>
 
       <View style={styles.content}>
-        <Text style={styles.title}>Welcome to Home</Text>
-        <Text style={styles.subtitle}>Search and review gyms near you.</Text>
+        <Text style={styles.title}>Welcome, {username}</Text>
+        <Text style={styles.subtitle}>{message}</Text>
       </View>
-    </SafeAreaView>
+      <View style={styles.gymList}>
+        {gyms.map((gym) => (
+        <TouchableOpacity key={gym.id} onPress={() => console.log(`Pressed ${gym.name}`)}>
+        <View key={gym.id} style={styles.gymCard}>
+          <Text style={styles.gymCardText}>{gym.name}</Text>
+          <Text style={styles.gymCardText}>{gym.address}</Text>
+          <Text style={styles.gymCardText}>Reviews: 0</Text>
+        </View>
+        </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
@@ -36,9 +68,26 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    backgroundColor: colors.lightGray,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  gymList: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20,
+  },
+  gymCard: {
+    width:'80%',
+    padding:16,
+    borderRadius:12,
+    marginTop:12,
+    backgroundColor: colors.green,
+  },
+  gymCardText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#fff',
   },
   title: {
     fontSize: 20,
